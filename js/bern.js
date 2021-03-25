a4c1ec92eaf8b66a5bcbe4f3c062be9367580b46
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-03-25 17:19:57 +0000

    updated dashboard users

--- js/bern.js
+++ js/bern.js
@@ -11,12 +11,12 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         var displayName = user.displayName;
         var email = user.email;
-        var emailVerified = user.emailVerified;
+        //var emailVerified = user.emailVerified;
         var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        console.log(email + " logged in")
+        //var isAnonymous = user.isAnonymous;
+        //var uid = user.uid;
+        //var providerData = user.providerData;
+        //console.log(email + " logged in")
 
         document.getElementById('uname').innerHTML = displayName;
         document.getElementById('displayName').innerHTML = displayName;
